9b41003cea3741372160bc39be8916def54f7fd8
Roman0064 <alee93474@gmail.com>
2023-10-03 07:55:01 +0300

    1

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -7,9 +7,9 @@ const schemaAdd = Joi.object({
 });
 
 const schemaUpd = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
 });
 
 module.exports = {
