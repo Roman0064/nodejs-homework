d371fc5b9bf88e61256aea7fd5cd6e41a78bce5b
Roman0064 <alee93474@gmail.com>
2023-10-15 21:56:05 +0300

    1

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,64 +1,64 @@
 const { Contact } = require('../models/contact');
 const { HttpError, ctrlWrapper } = require('../helpers');
 
-const getAll = async (req, res ) => {
-    const {_id: owner} = req.user;
-    const {page = 1, limit = 20, favorite} = req.query;
-    const skip = (page - 1) * limit;
-    const result = await Contact.find(favorite ? { owner, favorite } : { owner }, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
-    res.status(200).json(result);
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const query = favorite ? { owner, favorite } : { owner };
+  const result = await Contact.find(query, "-createdAt -updatedAt", { skip, limit }).populate("owner", "name email");
+  res.status(200).json(result);
 };
 
-const getContactById = async ( req, res ) => {
-    const {contactId} = req.params;
-    const result = await Contact.findById(contactId);
-    if(!result) {
-        throw HttpError(404, 'Not Found');
-    }
-    res.status(200).json(result);
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findOne({ _id: contactId, owner: req.user._id });
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.status(200).json(result);
 };
 
-const addContact = async ( req, res ) => {
-    const {_id: owner} = req.user;
-    const result = await Contact.create({...req.body, owner});
-    res.status(201).json(result);
+const addContact = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
 };
 
-const updateContact = async ( req, res ) => {
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-    if(!result) {
-        throw HttpError(404, 'Not Found');
-    }
-    res.status(200).json(result);
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findOneAndUpdate({ _id: contactId, owner: req.user._id }, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.status(200).json(result);
 };
 
-const updateStatusContact = async ( req, res ) => {
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-    if(!result) {
-        throw HttpError(404, 'Not Found');
-    }
-    res.status(200).json(result);
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findOneAndUpdate({ _id: contactId, owner: req.user._id }, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.status(200).json(result);
 };
 
-const deleteContacts = async ( req, res ) => {
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndDelete(contactId);
-    if(!result) {
-    throw HttpError(404, 'not Found');
-    }
-    res.status(200).json({
-        message: "contact deleted"
-    });
+const deleteContacts = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findOneAndDelete({ _id: contactId, owner: req.user._id });
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.status(200).json({
+    message: "contact deleted"
+  });
 };
 
 module.exports = {
-    getAll: ctrlWrapper(getAll),
-    getContactById: ctrlWrapper(getContactById),
-    addContact: ctrlWrapper(addContact),
-    updateContact: ctrlWrapper(updateContact),
-    updateStatusContact: ctrlWrapper(updateStatusContact),
-    deleteContacts: ctrlWrapper(deleteContacts),
+  getAll: ctrlWrapper(getAll),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  deleteContacts: ctrlWrapper(deleteContacts),
 };
-
